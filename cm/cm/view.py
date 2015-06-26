b9c9f2464bf430a430f00a60815568963bfaed1d
cwang <cwang@appannie.com>
2015-06-26 19:55:17 +0800

    add mock up data

--- cm/cm/view.py
+++ cm/cm/view.py
@@ -9,5 +9,22 @@ import json
 class HomeView(View):
 
     def get(self, *args, **kwargs):
-        test = {'name': 'cc', 'age': 1}
-        return render_to_response('home.html', {'test': mark_safe(json.dumps(test))})
+        test = []
+        for k in range(6):
+            tags = []
+            for i in range(28):
+                imgs = []
+                for j in range(100):
+                    imgObj = {
+                        'src': 'https://static-s.aa-cdn.net/img/ios/284882215/414fb5243cf13d547113e8741d51c3f2',
+                        'title': 'Facebook',
+                        'link': 'https://www.appannie.com/apps/ios/app/app-annie/'
+                    }
+                    imgs.append(imgObj)
+                tagObj = {
+                    'tagName': 'Test Tag %d' % i,
+                    'imgs': imgs
+                }
+                tags.append(tagObj)
+            test.append(tags)
+        return render_to_response('home.html', {'tagDatas': mark_safe(json.dumps(test))})
