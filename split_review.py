067d1cfd0e0b3f296c06773b929a9795cfcd5bd2
Demien Li <demien@appannie.com>
2015-06-26 11:37:21 +0800

    foo

---
+++ split_review.py
@@ -0,0 +1,24 @@
+def write_file(content, index):
+    file_name = 'reviews/top_n_app_tag_%s' % str(index)
+    with open(file_name, 'w') as output:
+        for line in content:
+            output.write(line)
+        output.close()
+
+
+if __name__ == '__main__':
+    last_split_index = 0
+    app_ids = set([])
+    write_content = []
+    with open('top_n_app_reviews') as f:
+        for line in f.readlines():
+            app_id, title, content = line.split('\t')
+            if app_ids:
+                app_ids = set(list(app_ids) + [app_id])
+            else:
+                app_ids = set([app_id])
+            if len(app_ids) % 200 == 0 and len(app_ids) != last_split_index and app_id != list(app_ids)[:1]:
+                last_split_index = len(app_ids)
+                write_file(write_content, last_split_index)
+                write_content = []
+            write_content.append(line)
--- tag.py
+++ tag.py
@@ -1,39 +1,69 @@
 import nltk
 import pprint
+import threading
 from collections import defaultdict, OrderedDict
 
 
-def get_tag(content, top_n=10):
-    result = defaultdict(int)
-    text = nltk.word_tokenize(content)
-    for tag, category in nltk.pos_tag(text):
-        if category in ['NN', 'NNP', 'NNS', 'NNPS']:  # http://www.monlp.com/2011/11/08/part-of-speech-tags/
-            result[tag] += 1
-    ordered_tags = OrderedDict(sorted(result.items(), key=lambda x: x[1], reverse=True)[:top_n])
-    return ordered_tags
+class Tag(threading.Thread):
+
+    def __init__(self, file_index_list, worker_cnt):
+        threading.Thread.__init__(self)  
+        self.file_index_list = file_index_list
+        self.worker_cnt = worker_cnt
+
+    def run(self):
+        for file_index in self.file_index_list:
+            self.tagging(str(file_index))
+
+    def tagging(self, file_index):
+        sort_app_tags = {}
+        app_tags = defaultdict(lambda: defaultdict(int))
+        index = 0
+        file_name = 'reviews/top_n_app_tag_%s' % file_index
+        with open(file_name) as f:
+            for line in f.readlines():
+                if index % 100 == 0:
+                    print 'Worker %s: The %s hundred line of file_index %s' % (str(self.worker_cnt), str(index / 100), file_index)
+                index += 1
+                app_id, title, content = line.split('\t')
+                try:
+                    result = self.get_tag(title + content)
+                except:
+                    continue
+                for tag, count in result.iteritems():
+                    app_tags[app_id][tag] += count
+        for app, tags in app_tags.iteritems():
+            sort_app_tags[app] = OrderedDict(sorted(tags.items(), key=lambda x: x[1], reverse=True))
+
+        output_file_name = 'tags/top_n_app_tag_%s' % self.file_index
+        with open(output_file_name, 'w') as output:
+            for app, tags in sort_app_tags.iteritems():
+                tag_str = ' '.join(['%s,%s' % (tag, count) for tag, count in tags.iteritems() if count > 1])
+                oneline = '%s\t%s\n' % (app, tag_str)
+                output.write(oneline)
+            output.close()
+
+    def get_tag(self, content, top_n=10):
+        result = defaultdict(int)
+        text = nltk.word_tokenize(content)
+        for tag, category in nltk.pos_tag(text):
+            if category in ['NN', 'NNP', 'NNS', 'NNPS']:  # http://www.monlp.com/2011/11/08/part-of-speech-tags/
+                result[tag] += 1
+        ordered_tags = OrderedDict(sorted(result.items(), key=lambda x: x[1], reverse=True)[:top_n])
+        return ordered_tags
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
 
 if __name__ == '__main__':
-    sort_app_tags = {}
-    app_tags = defaultdict(lambda: defaultdict(int))
-    index = 0
-    with open('top_n_app_reviews') as f:
-        for line in f.readlines()[:5000]:
-            if index % 100 == 0:
-                print 'The %s hundred line' % str(index / 100)
-            index += 1
-            app_id, title, content = line.split('\t')
-            try:
-                result = get_tag(title + content)
-            except:
-                continue
-            for tag, count in result.iteritems():
-                app_tags[app_id][tag] += count
-    for app, tags in app_tags.iteritems():
-        sort_app_tags[app] = OrderedDict(sorted(tags.items(), key=lambda x: x[1], reverse=True))
-
-    with open('top_n_app_tag', 'w') as output:
-        for app, tags in sort_app_tags.iteritems():
-            tag_str = ' '.join(['%s,%s' % (tag, count) for tag, count in tags.iteritems() if count > 1])
-            oneline = '%s\t%s\n' % (app, tag_str)
-            output.write(oneline)
-        output.close()
+    threading_cnt = 20 
+    index_list = range(200, 8401, 200)
+    split_index_list = list(chunks(index_list, len(index_list)/threading_cnt))
+    tag_workers = []
+    for index_list in split_index_list:
+        tag_workers.append(Tag(index_list, split_index_list.index(index_list)))
+    [tag_worker.start() for tag_worker in tag_workers]
