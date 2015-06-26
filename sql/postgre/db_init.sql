917baa7712d374cbb1be4b509289f4e46e37708c
pgchenliu <cliu@appannie.com>
2015-06-26 17:01:19 +0800

    Modify sql schema

--- sql/postgre/db_init.sql
+++ sql/postgre/db_init.sql
@@ -8,7 +8,7 @@ CREATE TABLE app (
   name character varying(512) NOT NULL,
   description character varying(5000) NOT NULL,
   icon character varying(512) NOT NULL,
-  weight INTEGER,
+  weight INTEGER
 );
 
 alter table app add CONSTRAINT app_pkey primary key (id);
@@ -21,7 +21,7 @@ CREATE TABLE tag_app_rel (
   id INTEGER NOT NULL,
   app_id INTEGER NOT NULL,
   tag_id INTEGER NOT NULL,
-  times INTEGER NOT NULL,
+  times INTEGER NOT NULL
 );
 
 alter table tag_app_rel add CONSTRAINT tag_app_rel_pkey primary key (id);
@@ -31,20 +31,18 @@ alter table tag_app_rel add CONSTRAINT tag_app_rel_pkey primary key (id);
 cm_tag table
 */
 CREATE TABLE cm_tag (
-  id INTEGER NOT NULL,
   app_id INTEGER NOT NULL,
-  times INTEGER NOT NULL,
-  tag INTEGER NOT NULL,
+  tag character varying(256) NOT NULL,
+  times INTEGER NOT NULL
 );
 
-alter table cm_tag add CONSTRAINT cm_tag_pkey primary key (id);
 
 /**
 tag table
 */
 CREATE TABLE tag (
   id INTEGER NOT NULL,
-  name character varying(512) NOT NULL,
+  name character varying(512) NOT NULL
 );
 
 alter table tag add CONSTRAINT tag_pkey primary key (id);
@@ -55,7 +53,7 @@ tag_similarity table
 CREATE TABLE tag_similarity (
   base_tag_id INTEGER NOT NULL,
   tag_id INTEGER NOT NULL,
-  similarity INTEGER NOT NULL,
+  similarity INTEGER NOT NULL
 );
 
 END;
