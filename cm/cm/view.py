ce8a7095f69f43f7b2ecbd34d1f5796a2ac3db0b
pgchenliu <cliu@appannie.com>
2015-06-27 10:43:03 +0800

    finish view

--- cm/cm/view.py
+++ cm/cm/view.py
@@ -1,31 +1,25 @@
-from django import http
-from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render_to_response
 from django.utils.safestring import mark_safe
 import json
+from .services import get_similar_tags, get_apps_by_tag
 
 
 class HomeView(View):
+    TOP_N_TAGS = ['iphone']
 
-    def get(self, *args, **kwargs):
-        test = []
-        for k in range(6):
-            tags = []
-            for i in range(28):
-                imgs = []
-                for j in range(100):
-                    imgObj = {
-                        'src': 'https://static-s.aa-cdn.net/img/ios/284882215/414fb5243cf13d547113e8741d51c3f2',
-                        'title': 'Facebook',
-                        'link': 'https://www.appannie.com/apps/ios/app/app-annie/'
-                    }
-                    imgs.append(imgObj)
+    def get(self, request, *args, **kwargs):
+        tag_objs = []
+        for tag in self.TOP_N_TAGS:
+            similar_tags = get_similar_tags(tag)
+            similar_tag_objs = []
+            for similar_tag in similar_tags:
+                app_objs = get_apps_by_tag(similar_tag)
                 tagObj = {
-                    'tagName': 'Test Tag %d' % i,
-                    'tagMessage': 'Tag message',
-                    'imgs': imgs
+                    'tagName': similar_tag[0],
+                    'tagMessage': '%s - %s - %s' % (app_objs[0]['title'], app_objs[1]['title'], app_objs[2]['title']),
+                    'imgs': app_objs
                 }
-                tags.append(tagObj)
-            test.append(tags)
-        return render_to_response('home.html', {'tagDatas': mark_safe(json.dumps(test))})
+                similar_tag_objs.append(tagObj)
+            tag_objs.append(similar_tag_objs)
+        return render_to_response('home.html', {'tagDatas': mark_safe(json.dumps(tag_objs))})
